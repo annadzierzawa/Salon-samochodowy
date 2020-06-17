8f8ef3ca88c785376aa8d0db1094417537be48c9
Kamil <kamilszpak99@gmail.com>
2020-06-17 22:00:46 +0200

    Implement basic repository model

--- DAL/Repozytoria/RepoSamochody.cs
+++ DAL/Repozytoria/RepoSamochody.cs
@@ -1,7 +1,72 @@
-﻿namespace Salon_samochodowy.DAL.Repozytoria
+﻿using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
+namespace Salon_samochodowy.DAL.Repozytoria
 {
+    using Encje;
     public class RepoSamochody
     {
-        
+        #region SQL_QUERIES
+        private const string WSZYSTKIE_SAMOCHODY = "SELECT * FROM Samochody";
+        private const string DODAJ_SAMOCHOD = "INSERT INTO `Samochody`(`nazwa`, `silnik`, `kolor`, `krajProdukcji`,`dataProdukcji`, `cenaModelu`) VALUES ";
+        private const string USUN_SAMOCHOD = "DELETE FROM `Samochody` WHERE idModelu=";
+        private const string USUN_SPRZEDAZE_SAMOCHODU = "DELETE FROM `Sprzedaz` WHERE idModelu=";
+        #endregion
+
+        #region CRUD
+
+
+        public static List<Samochod> PobierzWszystkieSamochody()
+        {
+            List<Samochod> samochody = new List<Samochod>();
+            using (var connection = DBConnection.Instance.Connection)
+            {
+                MySqlCommand command = new MySqlCommand(WSZYSTKIE_SAMOCHODY, connection);
+                connection.Open();
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                    samochody.Add(new Samochod(reader));
+                connection.Close();
+            }
+
+            return samochody;
+        }
+
+        public static bool DodajSamochodDoBazy(Samochod samochod)
+        {
+            bool stan = false;
+
+            using (var connection = DBConnection.Instance.Connection)
+            {
+                MySqlCommand command = new MySqlCommand($"{DODAJ_SAMOCHOD} {samochod.ToInsert()}", connection);
+                connection.Open();
+                var id = command.ExecuteNonQuery();
+                stan = true;
+                samochod.Id = (sbyte)command.LastInsertedId;
+                connection.Close();
+            }
+
+            return stan;
+        }
+
+        public static bool UsunSamochod(sbyte idSamochodu)
+        {
+            bool stan = false;
+            using (var connection = DBConnection.Instance.Connection)
+            {
+
+                MySqlCommand command1 = new MySqlCommand($"{USUN_SAMOCHOD} {idSamochodu}", connection);
+                MySqlCommand command2 = new MySqlCommand($"{USUN_SPRZEDAZE_SAMOCHODU} {idSamochodu}", connection);
+                connection.Open();
+                command1.ExecuteNonQuery();
+                command2.ExecuteNonQuery();
+                stan = true;
+                connection.Close();
+            }
+            return stan;
+        }
+        #endregion
+
+
     }
 }
--- DAL/Repozytoria/RepoSprzedazy.cs
+++ DAL/Repozytoria/RepoSprzedazy.cs
@@ -1,7 +1,64 @@
-﻿namespace Salon_samochodowy.DAL.Repozytoria
+﻿using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
+namespace Salon_samochodowy.DAL.Repozytoria
 {
+    using Encje;
     public class RepoSprzedazy
     {
-        
+
+        #region ZAPYTANIA
+        private const string WSZYSTKIE_SPRZEDAZE = "SELECT * FROM Sprzedaz";
+        private const string DODAJ_SPRZEDAZ = "INSERT INTO `Sprzedaz`(`idPracownika`, `idModelu`, `cena`) VALUES ";
+        private const string USUN_SPRZEDAZ = "DELETE FROM `Sprzedaz` WHERE idSprzedazy=";
+
+        #endregion
+
+        public static List<Sprzedaz> PobierzWszystkieSprzedaze()
+        {
+            List<Sprzedaz> sprzedaze = new List<Sprzedaz>();
+            using (var connection = DBConnection.Instance.Connection)
+            {
+                MySqlCommand command = new MySqlCommand(WSZYSTKIE_SPRZEDAZE, connection);
+                connection.Open();
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                    sprzedaze.Add(new Sprzedaz(reader));
+                connection.Close();
+            }
+            return sprzedaze;
+        }
+
+        public static bool DodajSprzedazDoBazy(Sprzedaz sprzedaz)
+        {
+            bool stan = false;
+            using (var connection = DBConnection.Instance.Connection)
+            {
+                MySqlCommand command = new MySqlCommand($"{DODAJ_SPRZEDAZ} {sprzedaz.ToInsert()}", connection);
+                connection.Open();
+                var id = command.ExecuteNonQuery();
+                stan = true;
+                sprzedaz.IdSprzedazy = (sbyte)command.LastInsertedId;
+                connection.Close();
+            }
+
+            return stan;
+        }
+
+        public static bool UsunSprzedaz(sbyte idSprzedazy)
+        {
+            bool stan = false;
+            using (var connection = DBConnection.Instance.Connection)
+            {
+
+                MySqlCommand command = new MySqlCommand($"{USUN_SPRZEDAZ} {idSprzedazy}", connection);
+                connection.Open();
+                command.ExecuteNonQuery();
+                stan = true;
+                connection.Close();
+            }
+            return stan;
+        }
+
     }
 }
