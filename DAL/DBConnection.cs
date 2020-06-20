a13ea8d7df0db13671fb5ceee0ea10f0f99604d6
Kamil <kamilszpak99@gmail.com>
2020-06-20 17:44:18 +0200

    Small fixes after merge
    Poprawienie inicjalizacji pakietu MaterialDesignXAML.
    Drobna poprawka w DBConnection.cs

--- DAL/DBConnection.cs
+++ DAL/DBConnection.cs
@@ -6,7 +6,7 @@ namespace Salon_samochodowy.DAL
     public class DBConnection
     {
         private readonly MySqlConnectionStringBuilder connStringBuilder = new MySqlConnectionStringBuilder();
-        
+
         private static DBConnection instance = null;
         public static DBConnection Instance => instance ?? (instance = new DBConnection());
 
@@ -14,10 +14,11 @@ namespace Salon_samochodowy.DAL
 
         private DBConnection()
         {
-            connStringBuilder.Server = Properties.Settings.Default.host; 
+            connStringBuilder.Server = Properties.Settings.Default.host;
             connStringBuilder.UserID = Properties.Settings.Default.user;
             connStringBuilder.Password = Properties.Settings.Default.password;
             connStringBuilder.Database = Properties.Settings.Default.database;
             connStringBuilder.Port = Properties.Settings.Default.port;
         }
+    }
 }
