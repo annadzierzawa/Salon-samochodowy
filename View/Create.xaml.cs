bfb95f862a3b2145b86e33fd2f862f928fdac1f9
Paulaner Silva <paulanersilva@gmail.com>
2020-06-23 21:57:51 +0200

    Hello

---
+++ View/Create.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Salon_samochodowy.View
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Create.xaml
+    /// </summary>
+    public partial class Create : UserControl
+    {
+        public Create()
+        {
+            InitializeComponent();
+        }
+    }
+}
