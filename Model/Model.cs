8617e16a2a67a8de073ed032bff0170a5e5552cb
Kamil <kamilszpak99@gmail.com>
2020-07-02 10:37:34 +0200

    Model Implementation

--- Model/Model.cs
+++ Model/Model.cs
@@ -1,8 +1,98 @@
-﻿namespace Salon_samochodowy
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using Salon_samochodowy.DAL.Encje;
+using Salon_samochodowy.DAL.Repozytoria;
+
+namespace Salon_samochodowy.Model
 {
     public class Model
     {
-        
+        //bazy danych
+        public ObservableCollection<Pracownik> Pracownicy { get; set; } = new ObservableCollection<Pracownik>();
+        public ObservableCollection<Samochod> Samochody { get; set; } = new ObservableCollection<Samochod>();
+        public ObservableCollection<Sprzedaz> Sprzedaze { get; set; } = new ObservableCollection<Sprzedaz>();
+
+        public Model()
+        {
+            //pobranie danych do kolekcjii
+            var pracownicy = RepoPracowników.PobierzWszystkichPracownikow();
+            var samochody = RepoSamochody.PobierzWszystkieSamochody();
+            var sprzedaze = RepoSprzedazy.PobierzWszystkieSprzedaze();
+
+            foreach (var p in pracownicy)
+            {
+                Pracownicy.Add(p);
+            }
+            foreach (var s in samochody)
+            {
+                Samochody.Add(s);
+            }
+            foreach (var sp in sprzedaze)
+            {
+                Sprzedaze.Add(sp);
+            }
+        }
+
+        private Pracownik ZnajdzPracownikaPoID(sbyte id)
+        {
+            return Pracownicy.FirstOrDefault(p => p.Id == id);
+        }
+
+        private Samochod ZnajdzSamochodPoID(sbyte id)
+        {
+            return Samochody.FirstOrDefault(s => s.Id == id);
+        }
+
+        private Sprzedaz ZnajdzSprzedazPoID(sbyte id)
+        {
+            return Sprzedaze.FirstOrDefault(sp => sp.IdSprzedazy == id);
+        }
+
+        public bool IfSprzedazInDB(Sprzedaz sprzedaz) => Sprzedaze.Contains(sprzedaz);
+
+        public bool DodajSprzedaz(Sprzedaz sprzedaz)
+        {
+            if (IfSprzedazInDB(sprzedaz)) return false;
+            if (!RepoSprzedazy.DodajSprzedazDoBazy(sprzedaz)) return false;
+            Sprzedaze.Add(sprzedaz);
+            return true;
+        }
+
+        public bool IfPracownikInDB(Pracownik pracownik) => Pracownicy.Contains(pracownik);
+
+        public bool DodajPracownika(Pracownik pracownik)
+        {
+            if (IfPracownikInDB(pracownik)) return false;
+            if (!RepoPracowników.DodajPracownikaDoBazy(pracownik)) return false;
+            Pracownicy.Add(pracownik);
+            return true;
+        }
+
+        public bool IfSamochodInDB(Samochod samochod) => Samochody.Contains(samochod);
+
+        public bool DodajSamochod(Samochod samochod)
+        {
+            if (IfSamochodInDB(samochod)) return false;
+            if (!RepoSamochody.DodajSamochodDoBazy(samochod)) return false;
+            Samochody.Add(samochod);
+            return true;
+        }
+
+        public bool EdytujPracownika(Pracownik pracownik, sbyte idPracownika)
+        {
+            if (!RepoPracowników.EdytujPracownika(pracownik, idPracownika)) return false;
+            for (int i = 0; i < Pracownicy.Count; i++)
+            {
+                if (Pracownicy[i].Id != idPracownika) continue;
+                pracownik.Id = idPracownika;
+                Pracownicy[i] = new Pracownik(pracownik);
+            }
+
+            return true;
+        }
+
+
+
     }
   
 }
